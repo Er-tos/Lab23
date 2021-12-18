f827f149fda7db0d94b73bbbd91c4a2d25c4e80c
Анатолий Ершов <a.v.ershov@sgugit.ru>
2021-12-18 17:39:18 +0700

    Добавьте файлы проекта.

---
+++ Lab23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, для которого необходимо рассчитать факториал.");
+            int numForFactorial = Convert.ToInt32(Console.ReadLine());
+            int fact = CalcFactorialAsync(numForFactorial).Result;
+            Console.WriteLine();
+            Console.Write("Факториал данного числа равен {0}", fact);
+            Console.ReadKey();
+        }
+        static async Task<int> CalcFactorialAsync(int num)
+        {
+            int result = await Task.Run(() => CalcFactorial(num));
+            return result;
+        }
+        static int CalcFactorial(int num)
+        {
+            int factorial = 1;
+
+            for (int i = 2; i < num + 1; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
